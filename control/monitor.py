70368c10b1dce9c8732ac9b47c1995843cacfab7
Juan A Avelino O <javelino2311@gmail.com>
2026-03-02 23:41:40 -0500

    Update cron schedule to run analyze_data every minute

--- control/monitor.py
+++ control/monitor.py
@@ -188,7 +188,7 @@ def start_cron():
     Inicia el cron que se encarga de ejecutar la función analyze_data cada 5 minutos.
     '''
     print("Iniciando cron...", flush=True)
-    schedule.every(2).minutes.do(analyze_data)
+    schedule.every(1).minutes.do(analyze_data)
     print("Servicio de control iniciado", flush=True)
     while 1:
         schedule.run_pending()
